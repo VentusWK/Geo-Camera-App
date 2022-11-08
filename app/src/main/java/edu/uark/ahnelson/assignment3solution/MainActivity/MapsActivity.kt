23269ad7941e9163bb56b980410c3106fea376a4
Piam Chittisane <pchittisane@gmail.com>
2022-11-08 15:06:32 -0600

    Camera functionality added

--- app/src/main/java/edu/uark/ahnelson/assignment3solution/MainActivity/MapsActivity.kt
+++ app/src/main/java/edu/uark/ahnelson/assignment3solution/MainActivity/MapsActivity.kt
@@ -1,15 +1,21 @@
 package edu.uark.ahnelson.assignment3solution.MainActivity
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.osmdroid.config.Configuration.*
 
@@ -18,10 +24,23 @@ import edu.uark.ahnelson.assignment3solution.GeoPhotoApplication
 import edu.uark.ahnelson.assignment3solution.R
 import edu.uark.ahnelson.assignment3solution.Util.replaceFragmentInActivity
 import org.osmdroid.util.GeoPoint
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MapsActivity : AppCompatActivity() {
 
     private lateinit var mapsFragment: OpenStreetMapFragment
+    var currentPhotoPath:String = ""
+
+    val takePictureResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(applicationContext, "No picture taken", Toast.LENGTH_LONG)
+        }else{
+            Log.d("MainActivity","Picture Taken at location $currentPhotoPath")
+        }
+    }
 
     val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
@@ -81,9 +100,31 @@ class MapsActivity : AppCompatActivity() {
         }
     }
 
+    private fun createFilePath(): String {
+        // Create an image file name
+        val timeStamp =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",  /* suffix */
+            storageDir /* directory */
+        )
+        // Save a file: path for use with ACTION_VIEW intent
+        return image.absolutePath
+    }
+
     private fun takeNewPhoto(){
-        //mapsFragment.clearMarkers()
-        mapsFragment.clearOneMarker(25)
+        val picIntent = Intent().setAction(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (picIntent.resolveActivity(packageManager) != null){
+            val filepath: String = createFilePath()
+            val myFile: File = File(filepath)
+            currentPhotoPath = filepath
+            val photoUri = FileProvider.getUriForFile(this,"edu.uark.ahnelson.assignment3solution.fileprovider",myFile)
+            picIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+            takePictureResultLauncher.launch(picIntent)
+        }
     }
 
     private fun checkForLocationPermission(){
